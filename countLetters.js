79865359a3bed591598266c6a5609130ff53570c
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-05-26 15:14:43 -0700

    countLetters.js file is added.

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(letters) {
+  const object = {};
+  for (let letter of letters) {
+    if (letter.match(/[a-z]/i)) {
+      if (object[letter]) {
+        object[letter] += 1;
+      } else {
+        object[letter] = 1;
+      }
+    }
+  }
+  return object;
+
+};
+console.log(countLetters("byeongjae kang"));
