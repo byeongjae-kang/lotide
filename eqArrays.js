9b109a31dc11b9da03f6cf3bb67364997d0bebee
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-05-25 21:17:00 -0700

    updated eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,4 @@
+// testing actual vs expected
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -5,20 +6,21 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
   }
 };
-
+// comparing arrays
 const eqArrays = function(array1, array2) {
-  let isEqual = true;
-  let moreIndex = (array1.length > array2.length) ? array1.length : array2.length;
-  for (let i = 0; i < moreIndex; i++) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      isEqual = false;
+      return false;
     }
   }
-  return isEqual;
+  return true;
 };
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+// testing samples
+console.log(eqArrays([1, 2, 3, 4], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3, 4], [3, 2, 1])); // => false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
