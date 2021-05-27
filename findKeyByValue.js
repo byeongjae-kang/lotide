2d6d8089daa2681fa0bed9d5a935b3b0aed688b6
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-05-26 18:09:10 -0700

    findKeyByValue.js is added.

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function(object, value) {
+  let property = undefined;
+  for (let key in object) {
+    if (object[key] === value) {
+      property = key;
+      break;
+    }
+  }
+  return property;
+};
+
+const bestTVShowsByGenre = {
+  // eslint-disable-next-line camelcase
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  drama2:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
