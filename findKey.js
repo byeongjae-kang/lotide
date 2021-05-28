ef3e7d8f132399eb8070b9ed3dcb875f1f689ffa
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-05-27 18:21:48 -0700

    findKey.js is added.

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, found) => {
+  for (let key in object) {
+    if (found(object[key])) {
+      return key;
+    }
+  }
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
