833f06dcb53a6456401909680cbc56a0fddee46a
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-05-31 14:54:43 -0700

    eqArrays.js file is refactored and created test file for this.

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// testing actual vs expected
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-// comparing arrays
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -18,11 +9,5 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
-// testing samples
-console.log(eqArrays([1, 2, 3, 4], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3, 4], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+module.exports = eqArrays;
