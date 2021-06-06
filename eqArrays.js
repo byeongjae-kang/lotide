00ae68a888319289ade276e9817c568adc5cdc09
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-06-06 14:31:16 -0700

    eqArray is updated.

--- eqArrays.js
+++ eqArrays.js
@@ -4,10 +4,14 @@ const eqArrays = function(array1, array2) {
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
+      if (Array.isArray(array1[i]) && eqArrays(array1[i], array2[i])) {
+        return true;
+      }
       return false;
     }
   }
   return true;
 };
+
 
 module.exports = eqArrays;
